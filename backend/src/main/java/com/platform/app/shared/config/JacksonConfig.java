c00ecbc6b56a7936d83b4d7ce1c797ef3bb6ec09
DamPhuQuy <phuquydam06@gmail.com>
2026-08-17 01:08:32 +0700

    fix application.yaml

--- backend/src/main/java/com/platform/app/shared/config/JacksonConfig.java
+++ backend/src/main/java/com/platform/app/shared/config/JacksonConfig.java
@@ -17,6 +17,7 @@ public class JacksonConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         return mapper;
     }
