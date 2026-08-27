17e6a29ad2b872d2e86f9cbd886720a2338ffa9c
DamPhuQuy <phuquydam06@gmail.com>
2026-08-27 12:30:31 +0700

    feat(backend): add swagger documentation annotation to HealthCheckResponse
    - add OpenAPI Tag annotation to HealthCheckResponse
    - ensure gradlew executable permissions

--- backend/src/main/java/com/platform/app/system/web/HealthCheckResponse.java
+++ backend/src/main/java/com/platform/app/system/web/HealthCheckResponse.java
@@ -1,16 +1,18 @@
 package com.platform.app.system.web;
 
+import java.time.Instant;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Tag(name = "HealthCheckResponse", description = "Response object for system health check")
 public class HealthCheckResponse {
 
     private String status;
